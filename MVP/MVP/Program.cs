f3442c23cbbfc35ea9013e6ef2ca1643fea4f50a
trupti <truptisharma105@gmail.com>
2023-01-11 10:26:38 +1300

    language function added

--- MVP/MVP/Program.cs
+++ MVP/MVP/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 //open browser
@@ -17,3 +18,37 @@ passwordTextbox.SendKeys("Trisha@105");
 // click on login button
 IWebElement loginButton = driver.FindElement(By.XPath("//button[text()='Login']"));
 loginButton.Click();
+Thread.Sleep(1000);
+//Check its right login
+IWebElement nameCheck = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/div[2]/div/span"));
+if (nameCheck.Text == "Hi Trupti")
+{
+    Console.WriteLine("Test successful");
+}
+else
+{
+    Console.WriteLine("Test failed");
+}
+//click on Languages
+IWebElement languagesTab = driver.FindElement(By.XPath("//h3[text()='Languages']"));
+languagesTab.Click();
+//click Add New
+IWebElement addnewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div"));
+addnewButton.Click();
+//get textbox for langaues and add value
+IWebElement addlangauesTextbox = driver.FindElement(By.Name("name"));
+addlangauesTextbox.SendKeys("english");
+//choose Language level from dropbox
+IWebElement chooselanguageDropbox = driver.FindElement(By.XPath("//select[@name='level']"));
+chooselanguageDropbox.Click();
+IWebElement basicOption = driver.FindElement(By.XPath("//select[@name='level']/option[text()='Basic']"));
+basicOption.Click();
+Thread.Sleep(500);
+//click add
+IWebElement addButton = driver.FindElement(By.XPath("//input[@type='button' and @value='Add']"));
+addButton.Click();
+//click on skills
+//click on Add New
+//add value in add skill textbox
+//select skill level from dropbox
+//click add
