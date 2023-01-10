a99e63ffe0b7732f70bd52ff30cbd93487979ba0
trupti <truptisharma105@gmail.com>
2023-01-11 12:59:03 +1300

    Add skill function added successfully

--- MVP/MVP/Program.cs
+++ MVP/MVP/Program.cs
@@ -20,7 +20,7 @@ IWebElement loginButton = driver.FindElement(By.XPath("//button[text()='Login']"
 loginButton.Click();
 Thread.Sleep(1000);
 //Check its right login
-IWebElement nameCheck = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/div[2]/div/span"));
+IWebElement nameCheck = driver.FindElement(By.XPath("//span[@class='item ui dropdown link ']"));
 if (nameCheck.Text == "Hi Trupti")
 {
     Console.WriteLine("Test successful");
@@ -45,10 +45,22 @@ IWebElement basicOption = driver.FindElement(By.XPath("//select[@name='level']/o
 basicOption.Click();
 Thread.Sleep(500);
 //click add
-IWebElement addButton = driver.FindElement(By.XPath("//input[@type='button' and @value='Add']"));
-addButton.Click();
+IWebElement addlanguageButton = driver.FindElement(By.XPath("//input[@type='button' and @value='Add']"));
+addlanguageButton.Click();
 //click on skills
+IWebElement skillsTab = driver.FindElement(By.XPath("//a[text()='Skills']"));
+skillsTab.Click();
 //click on Add New
+IWebElement addnewskillButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+addnewskillButton.Click();
 //add value in add skill textbox
+IWebElement addskillTextbox = driver.FindElement(By.XPath("//input[@type='text' and @name='name']"));
+addskillTextbox.SendKeys("c#");
 //select skill level from dropbox
+IWebElement chooseskillDropbox = driver.FindElement(By.XPath("//select[@name='level']"));
+chooseskillDropbox.Click();
+IWebElement beginnerOption = driver.FindElement(By.XPath("//select[@name='level']/option[@value='Beginner']"));
+beginnerOption.Click();
 //click add
+IWebElement addskillButton = driver.FindElement(By.XPath("//input[@type='button' and @value='Add']"));
+addskillButton.Click();
